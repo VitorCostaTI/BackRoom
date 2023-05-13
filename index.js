59605d3230913255a15cea55a19eaa81174dfc85
Vitor Costa <vitortecnologia62@gmail.com>
2023-05-13 15:32:48 -0300

    Organização da arquitetura no software

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
+// Inicialização App
+
 const express = require('express');
 const app = express();
 
+// Logo
+
 const logo = require('./Assets/js/logo');
 
-let port = 8080;
+// Conexão com DB
 
-const bodyParser = require('body-parser');
+const connection = require('./Config/database'); 
 
-app.set('view engine', 'ejs');
+const Models = require('./Model/Models'); 
 
-const connection = require('./Config/database');
-
-const Models = require('./Model/Models')
-
-connection.authenticate().then(() =>{
-    console.log("Banco conectado com sucesso!!")
+connection.authenticate().then(() => {
+    console.log("Banco conectado com sucesso!!");
 }).catch(() => {
-    console.log("Falha ao conectar no banco de dados!!")
-})
+    console.log("Falha ao conectar no banco de dados!!");
+});
+
+// Rotas
+
+const auditoriaRoute = require('./Routes/api/AuditoriaRouter');
+
+app.use('/api/auditoria', auditoriaRoute);
+
+// Configurações do servidor
+
+let port = 8080;
 
 app.listen(port, ()=>{
     console.log("");
